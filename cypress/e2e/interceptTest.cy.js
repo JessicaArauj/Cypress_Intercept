dfb0b5ee1e92340be740234981e3bd2477b59a3a
Jessica Teixeira Araujo <jessicateixeiraaraujo@gmail.com>
2023-05-06 14:06:45 -0300

    Script de teste com o método Intercept

--- cypress/e2e/interceptTest.cy.js
+++ cypress/e2e/interceptTest.cy.js
@@ -1,15 +1,37 @@
-/// <reference types = "cypress" />
-import {homePage, productSearchPAge} from '../support/pages'
-const data = require('../fixture/data.json')
+/// <reference types="cypress" />
+import { homePage, productSearchPage } from '../support/pages'
+const data = require('../fixtures/data.json')
 
 describe('Product Search', () => {
-    beforeEach(() => {
-        cy.visit("/")
-    });
 
-    it('autocomplete product item should be return correctly', () => {
-        homePage.searchMagnifier()
-        productSearchPAge.search('Jacket')
-        productSearchPAge.productList.first().should('have.attr', 'title', 'Ingrid Running Jacket')
-    });
-});
+  before(()=>{
+    cy.intercept({
+      method: 'GET',
+      url: '/wp-admin/admin-ajax*',
+      query: {
+        term: 'Jacket'
+      }
+    }, req => {
+      req.reply(
+        {
+          statusCode: 200,
+          body: `${req.query.callback}(
+            ${JSON.stringify(
+              data.autocompleteSearchData
+            )} 
+          )`
+        }
+      )
+    })    
+  })
+
+  beforeEach(() => {
+    cy.visit("/")
+  })
+
+  it('autocomplete product item should be return correctly', () => {
+    homePage.searchMagnifier()
+    productSearchPage.search('Jacket')
+    productSearchPage.productList.first().should('have.attr', 'title', 'Ingrid Running Jacket')
+  })
+})
